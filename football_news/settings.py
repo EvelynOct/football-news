a71525f2a7e1b76a18aaf4a561a732e7c999a871
EvelynOct <evelyne.aritonang72@gmail.com>
2025-09-10 13:56:07 +0700

    Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,6 +34,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "evelyne-octaviana-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "<your-pws-deployment-url>"
+]
 
 # Application definition
 
@@ -62,7 +65,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
